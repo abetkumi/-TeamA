efd8f6016f33c5430b894784b96d46b8a8bacfc7
abetkumi <ca01234001@st.kawahara.ac.jp>
2024-04-19 15:45:28 +0900

    ship_gauge

--- k2Engine-master/GameTemplate/Game/Boat.h
+++ k2Engine-master/GameTemplate/Game/Boat.h
@@ -9,13 +9,16 @@ public:
 	void Update();
 	void Render(RenderContext& rc);
 	void Move();
+	void ShipMove();
 
 	ModelRender m_modelRender;
+	SpriteRender m_spriteRender;
 	PhysicsStaticObject m_physicsStaticObject;
 
 	Vector3 m_position;
 	Vector3 m_scale;
-
+	Vector3 m_shipGauge;
+	Vector3 m_shipposition;
 	Player* player;
 };
 

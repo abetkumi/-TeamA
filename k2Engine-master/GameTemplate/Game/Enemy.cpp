371c57bd1fa27dfe78bd231edd9f346c7535e70c
rin3432 <nabi1201@icloud.com>
2024-04-27 09:23:18 +0900

    a1

--- k2Engine-master/GameTemplate/Game/Enemy.cpp
+++ k2Engine-master/GameTemplate/Game/Enemy.cpp
@@ -3,10 +3,16 @@
 #include "Player.h"
 #include "Arrow.h"
 
+#include <iostream>
+#include <chrono>
+#include <thread>
+
 Enemy::Enemy()
 {
 	m_modelRender.Init("Assets/modelData/enemy.tkm");
 	player = FindGO<Player>("player");
+
+	arrowtimer = arrowtime;
 
 	//キャラクターコントローラーの初期化処理しないとエラー吐かれるので注意
 	m_charaCon.Init(0.0f, 0.0f, Vector3::Zero);
@@ -21,6 +27,7 @@ void Enemy::Update()
 {
 	Rotation();
 
+	Arrow1();
 	/*if (g_pad[0]->IsTrigger(enButtonB))
 	{
 		arrow = NewGO<Arrow>(0, "arrow");
@@ -40,16 +47,44 @@ void Enemy::Render(RenderContext& rc)
 
 void Enemy::Rotation()
 {
+	/*if (v.LengthSq() <= 300 * 300)
+	{
+		return true;
+	}*/
 	Vector3 v = player -> m_position - m_position;
 	v.Normalize();
-	float distToPlayer = v.Length();
+	float distToPlayer = v.LengthSq();
 	if (distToPlayer < 30)
 	{
 		m_moveSpeed = v * 100.0f;
 		//m_position += m_moveSpeed;
-		m_modelRender.SetPosition(m_position);
 	}
 
+	/*Vector3 diff = player->m_position - m_position;
+	if (diff.LengthSq() <= 30.0f)
+	{
+		m_moveSpeed = diff * 100.0f;
+	}*/
+
+
+	m_modelRender.SetPosition(m_position);
 	m_rotation.SetRotationYFromDirectionXZ(m_moveSpeed);
 	m_modelRender.SetRotation(m_rotation);
+}
+
+void Enemy::Arrow1()
+{
+	if (arrowtimer > 0)
+	{
+		arrowtimer -= g_gameTime->GetFrameDeltaTime();
+		return;
+	}
+
+	arrow = NewGO<Arrow>(0, "arrow");
+	arrow->m_position = (m_position);
+	arrow->m_1stPosition = arrow->m_position;
+	arrow->m_rotation = m_rotation;
+
+
+	arrowtimer = arrowtime;
 }
--- k2Engine-master/GameTemplate/Game/Enemy.h
+++ k2Engine-master/GameTemplate/Game/Enemy.h
@@ -10,7 +10,8 @@ public:
 	void Update();
 	void Render(RenderContext& rc);
 	void Rotation();
-
+	void Arrow1();
+	
 	Vector3 m_position;
 	Vector3 m_scale = Vector3::One;
 	Vector3 m_moveSpeed;
@@ -24,5 +25,7 @@ public:
 	Player* player;
 	Arrow* arrow;
 	
+	float arrowtimer = 0.0f;
+	float arrowtime = 3.0f;
 };
 

3c1be3d6e308eb88a7f755a2dddfa8c523d2114d
abetkumi <ca01234001@st.kawahara.ac.jp>
2024-04-27 15:46:39 +0900

    ship_move

--- k2Engine-master/GameTemplate/Game/Player.cpp
+++ k2Engine-master/GameTemplate/Game/Player.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Player.h"
 #include "Arrow.h"
+#include "Game.h"
 
 Player::Player()
 {
@@ -8,6 +9,8 @@ Player::Player()
 	m_charaCon.Init(25.0f, 75.0f, m_position);
 
 	HP = 100;
+	game = FindGO<Game>("game");
+
 }
 
 Player::~Player()
@@ -34,50 +37,64 @@ void Player::Update()
 
 void Player::Move()
 {
-	m_moveSpeed.x = 0.0f;
-	m_moveSpeed.z = 0.0f;
+	//m_moveSpeed.x = 0.0f;
+	//m_moveSpeed.z = 0.0f;
 
-	Vector3 stickL;
-	stickL.x = g_pad[0]->GetLStickXF();
-	stickL.y = g_pad[0]->GetLStickYF();
+	////通常の移動モーション
+	//Vector3 stickL;
+	//stickL.x = g_pad[0]->GetLStickXF();
+	//stickL.y = g_pad[0]->GetLStickYF();
 
-	Vector3 forward = g_camera3D->GetForward();
-	Vector3 right = g_camera3D->GetRight();
+	//Vector3 forward = g_camera3D->GetForward();
+	//Vector3 right = g_camera3D->GetRight();
 
-	forward.y = 0.0f;
-	right.y = 0.0f;
+	//forward.y = 0.0f;
+	//right.y = 0.0f;
 
-	right *= stickL.x * 120.0f;
-	forward *= stickL.y * 120.0f;
+	//right *= stickL.x * 120.0f;
+	//forward *= stickL.y * 120.0f;
 
-	m_moveSpeed += right + forward;
+	//m_moveSpeed += right + forward;
 
-	if (m_charaCon.IsOnGround())
+	game->m_pointPosition = game->path00_pointList[point];
+	Vector3 diff = game->m_pointPosition - m_position;
+	
+	float disToPlayer = diff.Length();
+	if (disToPlayer <= 10.0f)
 	{
-		m_moveSpeed.y = 0.0f;
+		point++;
 	}
-	else
-	{
-		m_moveSpeed.y -= 10.0f;
-	}
+	diff.Normalize();
+	
+
+	m_moveSpeed = diff * 10.0f;
+	//if (m_charaCon.IsOnGround())
+	//{
+	//	m_moveSpeed.y = 0.0f;
+	//}
+	//else
+	//{
+	//	m_moveSpeed.y -= 10.0f;
+	//}
 	
 	
-	
-if (g_pad[0]->IsPress(enButtonA))
-	{
-		m_moveSpeed.y = 300.0f;
-	}
-	if (g_pad[0]->IsPress(enButtonX))
-	{
-		m_moveSpeed = (right + forward) * 7.5;
-	}
+	//ダッシュとジャンプ
+//if (g_pad[0]->IsPress(enButtonA))
+//	{
+//		m_moveSpeed.y = 300.0f;
+//	}
+//	if (g_pad[0]->IsPress(enButtonX))
+//	{
+//		m_moveSpeed = (right + forward) * 7.5;
+//	}
+	//強制ゲームオーバーコマンド
 	if (g_pad[0]->IsPress(enButtonY))
 	{
 		HP -= 100;
 	}
 
 	m_position = m_charaCon.Execute(m_moveSpeed, 1.0f / 20.0f);//大まかな移動速度
-
+	//m_position = m_moveSpeed;
 	m_modelRender.SetPosition(m_position);
 }
 

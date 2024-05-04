6ee6498d0901a795394ee3abe93984785df1d3eb
rin3432 <nabi1201@icloud.com>
2024-05-04 13:46:21 +0900

    arata

--- k2Engine-master/GameTemplate/Game/Player.cpp
+++ k2Engine-master/GameTemplate/Game/Player.cpp
@@ -38,8 +38,7 @@ void Player::Update()
 		arrow->m_position = (m_position + corre2);
 		arrow->m_1stPosition = arrow->m_position;
 		arrow->m_rotation = m_rotation;
-
-		arrow->SetEnArrow(Arrow::enArrow_Enemy);
+		arrow->SetEnArrow(Arrow::enArrow_Player);
 	}
 }
 

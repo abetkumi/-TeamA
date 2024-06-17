381af275065b1fd61d09693810d521020ed67d1c
abetkumi <ca01234001@st.kawahara.ac.jp>
2024-06-17 10:24:59 +0900

    岩の当たり判定完成

--- k2Engine-master/GameTemplate/Game/Rock.cpp
+++ k2Engine-master/GameTemplate/Game/Rock.cpp
@@ -4,6 +4,8 @@
 #include "Game.h"
 #include "collision/CollisionObject.h"
 
+#define kyori 500.0f * 500.0f
+
 namespace
 {
 	const Vector3 rock1 = { 0.0f,0.0f,0.0f };
@@ -16,7 +18,7 @@ Rock::Rock()
 
 Rock::~Rock()
 {
-	DeleteGO(r_collisionObject);
+
 }
 
 bool Rock::Start()
@@ -28,12 +30,7 @@ bool Rock::Start()
 
 	Vector3 hoge = { 0.0f,0.0f,0.0f };
 
-	r_collisionObject = NewGO<CollisionObject>(3);
-	r_collisionObject->CreateMesh(hoge, r_rot, r_modelRender.GetModel(), r_modelRender.GetModel().GetWorldMatrix());
-	r_collisionObject->SetName("r_rock");
-	r_collisionObject->SetIsEnableAutoDelete(false);
-	r_collisionObject->Update();
-	//r_physicsStaticObject.CreateFromModel(r_modelRender.GetModel(), r_modelRender.GetModel().GetWorldMatrix());
+	r_physicsStaticObject.CreateFromModel(r_modelRender.GetModel(), r_modelRender.GetModel().GetWorldMatrix());
 
 	player = FindGO<Player>("player");
 	game = FindGO<Game>("game");
@@ -41,29 +38,39 @@ bool Rock::Start()
 	return true;
 }
 
-void Rock::Colision()
+void Rock::RockDamage()
 {
-	const auto& collisions = g_collisionObjectManager->FindCollisionObjects("r_rock");
-
-	for (auto collision : collisions) {
-		if (collision->IsHit(player->m_charaCon))
-		{
-			player->m_arrowState = 3;
-			player->HP -= 5;
-			player->m_position = game->path00_pointList[player->m_point - 1];
-			r_rockFlag = true;
-		}
-	}
 	if (r_rockFlag == true)
 	{
-		DeleteGO(r_collisionObject);
+		r_damageLag += g_gameTime->GetFrameDeltaTime();
+		if (r_damageLag > 2.0f)
+		{
+			r_rockFlag = false;
+		}
+		return;
+	}
+	else
+	{
 		r_rockFlag = false;
+	}
+
+	Vector3 diff = r_position - player->m_position;
+	float distToPlayer = diff.Length();
+	if (distToPlayer < 400)
+	{
+		r_rockFlag = true;
+		player->HP -= 5;
+		player->m_arrowState = 3;
+		player->m_position = game->path01_pointList[player->m_point - 2];
+		player->m_modelRender.SetPosition(player->m_position);
+		player->m_charaCon.SetPosition(player->m_position);
+		player->m_modelRender.Update();
 	}
 }
 
 void Rock::Update()
 {
-	Colision();
+	RockDamage();
 	r_modelRender.Update();
 }
 
@@ -71,3 +78,13 @@ void Rock::Render(RenderContext& rc)
 {
 	r_modelRender.Draw(rc);
 }
+
+//const bool Rock::rRock()
+//{
+//Vector3 diff = player->m_position - r_position;
+//	if (diff.LengthSq() >= kyori)
+//	{
+//		return true;
+//	}
+//	return false;
+//}
